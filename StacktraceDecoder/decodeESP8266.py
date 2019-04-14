d88dc46f02187e99278a8309d5fccfcd6d44cb8e
Tederean <tederean@gmail.com>
2019-04-14 13:01:34 +0200

    Update

---
+++ StacktraceDecoder/decodeESP8266.py
@@ -0,0 +1,11 @@
+#!/usr/local/bin/python3.7
+
+import subprocess
+import os
+
+subprocess.call([
+    'java', '-jar', 'StacktraceDecoder.jar',
+    os.path.abspath('~/.platformio/packages/toolchain-xtensa/bin/xtensa-lx106-elf-addr2line'),
+    os.path.abspath('../.pioenvs/nodemcuv2/firmware.elf'),
+    os.path.abspath('StacktraceDecoder.txt')
+])
